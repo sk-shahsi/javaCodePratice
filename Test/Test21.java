a718522b3d86649d9878b30a3207afa5579f09f2
ErAashishRanjan <er.aashishranjan@gmail.com>
2025-02-18 11:42:13 +0530

    find max in array

---
+++ Test/Test21.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Test21 {
+    public static void main(String[] args) {
+        int arr[]={11,5,6};
+        int first=arr[0];
+        int last=arr[2];
+        if (first>last){
+            System.out.println("first"+first);
+        }else {
+            System.out.println("last"+last);
+        }
+    }
+}
