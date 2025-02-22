61e7ae11769928eb3a18fcf9fd569d464641b6b1
ErAashishRanjan <er.aashishranjan@gmail.com>
2025-02-22 18:36:02 +0530

    find max in array

--- Test/Test22.java
+++ Test/Test22.java
@@ -1,7 +1,22 @@
 package Test;
 
+import java.util.*;
+
 public class Test22 {
+    private static int[] printArray(int size){
+        int[] arr = new int[size];
+        int count = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (count<= size){
+                arr[i]= count;
+                count++;
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args) {
+
+        System.out.println(Arrays.toString( printArray(5)));
 
     }
 }
---
+++ Test/Test23.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Test23 {
+    public static void main(String[] args) {
+
+    }
+
+}
